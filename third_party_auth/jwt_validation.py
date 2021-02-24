24211f458fa6b8dca2de3aaecf9a6352865dc2bb
Geoff Von Allmen <geoff@ibleducation.com>
2021-02-24 21:28:11 +0000

    Check iss_aud_iat

--- third_party_auth/jwt_validation.py
+++ third_party_auth/jwt_validation.py
@@ -1,3 +1,4 @@
+import time
 import logging
 
 from Cryptodome.PublicKey.RSA import importKey
@@ -8,8 +9,10 @@ from jwkest.jws import (
     BadSignature,
     NoSuitableSigningKeys
 )
+from social_django.models import UserSocialAuth
 
 log = logging.getLogger(__name__)
+
 BEGIN_KEY = "-----BEGIN PUBLIC KEY-----"
 END_KEY = "-----END PUBLIC KEY-----"
 
@@ -33,9 +36,17 @@ def validate_jwt(provider, token):
     pub_key_str = provider.get_setting("PUBLIC_KEY")
     payload = _check_signature(pub_key_str, token)
 
+    try:
+        social_auth = UserSocialAuth.objects.get(uid=payload['sub'])
+    except UserSocialAuth.DoesNotExist:
+        raise JwtValidationError(
+            'Unable to find Social Auth User: %s', payload['sub'])
+
     # TODO: Fetch last id token for current user
-    last_id_token = ''
-    _check_iss_aud_iat(payload)
+    last_id_token = _check_signature(
+        pub_key_str, social_auth.extra_data.get('access_token'))
+
+    _check_iss_aud_iat(provider, payload)
     _check_sub_sid(payload)
     _check_events_claim(payload)
     _check_nonce_not_present(payload)
@@ -43,6 +54,7 @@ def validate_jwt(provider, token):
     _check_iss(last_id_token, payload)
     _check_sub(last_id_token, payload)
     _check_sid(last_id_token, payload)
+    return payload
 
 
 def _check_signature(pub_key_str, token):
@@ -64,13 +76,64 @@ def _check_signature(pub_key_str, token):
     return payload
 
 
-def _check_iss_aud_iat(payload):
+def _check_iss_aud_iat(provider, payload):
     """Validate iss, aud and iat claims
 
     Step 3. Validate the iss, aud, and iat Claims in the same way they are
     validated in ID Tokens.
     """
-    pass
+    iss = provider.get_setting('ISS')
+    client_id = provider.key
+
+    # Must contain these claims
+    keys = set(('iss', 'iat', 'aud'))
+    diff = keys - set(payload.keys())
+    if diff:
+        raise JwtValidationError('Missing JWT Claims: %s', diff)
+
+    _check_iat(payload['iat'])
+    _check_iss(iss, payload['iss'])
+    _check_aud(client_id, payload['aud'], payload.get('azp', None))
+
+
+def _check_aud(client_id, aud, azp):
+    """Validate aud claim"""
+    # Audience could be a list
+    if not isinstance(aud, list):
+        aud = [aud]
+
+    if client_id not in aud:
+        raise JwtValidationError(
+            'Provider client_id (%s) not in aud (%s)', client_id, aud
+        )
+
+    if azp is not None and client_id != azp:
+        raise JwtValidationError(
+            'azp claim present (%s) but does not equal provider client_id '
+            '(%s)', azp, client_id)
+
+
+def _check_iss(iss, payload):
+    """Validate ISS claim"""
+    if iss != payload['iss']:
+        raise JwtValidationError(
+            "JWT iss claim (%s) does not match provider iss (%s)",
+            payload['iss'], iss
+        )
+
+
+def _check_iat(iat, slack=120):
+    """Validate iat claim
+
+    Args:
+        iat (int): Time token was created
+        slack (int): Max seconds in past token must have been created
+    """
+    now = int(time.time())
+    if (now - iat) > slack:
+        raise JwtValidationError(
+            "Token created too long in the past (%ss ago > %s); rejecting",
+            now - iat, slack)
 
 
 def _check_sub_sid(payload):
