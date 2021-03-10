ecd6244c52be0a894fa5ad68cbfc6aa024041c65
Geoff Von Allmen <geoff@ibleducation.com>
2021-03-10 19:45:51 +0000

    Test LogoutOfSessions

--- third_party_auth/tests/test_backchannel_logout.py
+++ third_party_auth/tests/test_backchannel_logout.py
@@ -2,9 +2,11 @@ import json
 import mock
 import pytest
 
+from django.conf import settings
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.test import RequestFactory, TestCase
+from django.contrib.auth import login
+from django.test import RequestFactory, TestCase, override_settings
 from third_party_auth.tests.testutil import ThirdPartyAuthTestMixin
 
 from crum import CurrentRequestUserMiddleware
@@ -16,6 +18,9 @@ from third_party_auth import backchannel_logout as bcl
 from third_party_auth.jwt_validation import JwtValidationError
 
 from social_django.models import UserSocialAuth
+
+LMS_FEATURES = settings.FEATURES.copy()
+LMS_FEATURES['PREVENT_CONCURRENT_LOGINS'] = True
 
 
 @pytest.fixture(autouse=True)
@@ -35,8 +40,12 @@ class BaseTestCase(TestCase, ThirdPartyAuthTestMixin):
     def setUpClass(cls):
         super(BaseTestCase, cls).setUpClass()
         cls.site = SiteFactory(domain='0.testserver.fake')
-        cls.user = UserFactory()
         cls.factory = RequestFactory()
+        cls.url = reverse('tpa-backchannel-logout', kwargs={'backend': 'keycloak'})
+        cls.provider = 'keycloak'
+
+    def setUp(self):
+        self.user = UserFactory()
 
     def _setup_request(self, path, post_dict):
         self.request = self.factory.post(path, post_dict)
@@ -95,19 +104,6 @@ def test_get_profile_from_sub_raises_multiple_found(user):
 
 
 @pytest.mark.skip
-def test_logout_of_sessions_no_sessions_exist(user):
-    """If no sessions exist, message logged and none are deleted"""
-    sessions = {'session_id': None, 'cms_session_id': None}
-    request = RequestFactory()
-    bcl._logout_of_sessions(sessions, )
-
-
-@pytest.mark.skip
-def test_logout_of_sessions_all_sessions_deleted():
-    pass
-
-
-@pytest.mark.skip
 def test_get_backchannel_logout_response_sets_status():
     pass
 
@@ -119,12 +115,6 @@ def test_get_current_provider():
 
 @mock.patch('third_party_auth.provider._PSA_OAUTH2_BACKENDS', ['keycloak'])
 class TestBackchannelLogoutView(BaseTestCase):
-
-    @classmethod
-    def setUpClass(cls):
-        super(TestBackchannelLogoutView, cls).setUpClass()
-        cls.url = reverse('tpa-backchannel-logout', kwargs={'backend': 'keycloak'})
-        cls.provider = 'keycloak'
 
     def test_token_not_provided_returns_400(self):
         """Test no logout_token provided then returns 400"""
@@ -201,7 +191,7 @@ class TestBackchannelLogoutView(BaseTestCase):
 
     @mock.patch('third_party_auth.backchannel_logout._get_user_from_sub')
     @mock.patch('third_party_auth.backchannel_logout.jwt_validation.validate_jwt')
-    def test_no_active_sessions_profile_unchanged_returns_200(self, mock_jwt_val, mock_user_sub):
+    def test_logout_of_sessions_succeeds_returns_200(self, mock_jwt_val, mock_user_sub):
         """If user has no active sessions, returns 200 and profile meta not changed"""
         mock_jwt_val.return_value = {'sub': 'f:something:not_found@test.com'}
         mock_user_sub.return_value = self.user
@@ -209,4 +199,49 @@ class TestBackchannelLogoutView(BaseTestCase):
         self._setup_request(self.url, {'logout_token': 'something'})
         resp = bcl.back_channel_logout(self.request, self.provider)
         assert resp.status_code == 200
+
+
+class TestLogoutOfSessions(BaseTestCase):
+
+    @override_settings(FEATURES=LMS_FEATURES)
+    def test_no_active_sessions_profile_not_touched(self):
+        """If no active sessions, user's profile not changed and warning logged"""
+        self._setup_request(self.url, {'logout_token': 'something'})
         assert self.user.profile.get_meta() == {}
+
+        bcl._logout_of_sessions(self.user, self.request)
+        self.user.profile.refresh_from_db()
+        assert self.user.profile.get_meta() == {}
+        assert "Logout request sent" in self._caplog.messages[-1]
+
+    @override_settings(FEATURES=LMS_FEATURES)
+    @mock.patch('third_party_auth.backchannel_logout.user_logged_out')
+    def test_lms_active_session_is_removed(self, mock_logged_out):
+        """If active LMS session, it's removed from profile and user logged out"""
+        self.client.force_login(self.user)
+        # Client now has a session in the lms
+        self.user.profile.refresh_from_db()
+        assert self.user.profile.get_meta().get('session_id') is not None
+        assert 'cms_session_id' not in self.user.profile.get_meta()
+
+        self._setup_request(self.url, {'logout_token': 'something'})
+        bcl._logout_of_sessions(self.user, self.request)
+
+        mock_logged_out.send.assert_called_once()
+        assert self.user.profile.get_meta() == {'session_id': None}
+
+    @override_settings(IBL_CMS_PREVENT_CONCURRENT_LOGINS=True)
+    @mock.patch('third_party_auth.backchannel_logout.user_logged_out')
+    def test_cms_active_session_is_removed(self, mock_logged_out):
+        """If active CMS session, it's removed from profile and user logged out"""
+        self.client.force_login(self.user)
+        self.user.profile.refresh_from_db()
+        # Client now has a session in the cms
+        assert self.user.profile.get_meta().get('cms_session_id') is not None
+        assert 'session_id' not in self.user.profile.get_meta()
+
+        self._setup_request(self.url, {'logout_token': 'something'})
+        bcl._logout_of_sessions(self.user, self.request)
+
+        mock_logged_out.send.assert_called_once()
+        assert self.user.profile.get_meta() == {'cms_session_id': None}
