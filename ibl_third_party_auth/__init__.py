c73940c9bdb2be8931b7e355578c763b74d80df8
Geoff Von Allmen <geoff@ibleducation.com>
2021-06-07 21:21:40 +0000

    Add provider_id patch

--- ibl_third_party_auth/__init__.py
+++ ibl_third_party_auth/__init__.py
@@ -1,18 +1,19 @@
 """Third party authentication. """
 
+default_app_config = 'ibl_third_party_auth.apps.IBLThirdPartyAuthConfig'
 
-from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+# from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 
-default_app_config = 'common.djangoapps.third_party_auth.apps.ThirdPartyAuthConfig'
+# default_app_config = 'common.djangoapps.third_party_auth.apps.ThirdPartyAuthConfig'
 
 
-def is_enabled():
-    """Check whether third party authentication has been enabled. """
+# def is_enabled():
+#     """Check whether third party authentication has been enabled. """
 
-    # We do this import internally to avoid initializing settings prematurely
-    from django.conf import settings
+#     # We do this import internally to avoid initializing settings prematurely
+#     from django.conf import settings
 
-    return configuration_helpers.get_value(
-        "ENABLE_THIRD_PARTY_AUTH",
-        settings.FEATURES.get("ENABLE_THIRD_PARTY_AUTH")
-    )
+#     return configuration_helpers.get_value(
+#         "ENABLE_THIRD_PARTY_AUTH",
+#         settings.FEATURES.get("ENABLE_THIRD_PARTY_AUTH")
+#     )
