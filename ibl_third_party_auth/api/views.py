d58bbe4eae3ed6f6248144c2f0caf0876d5ce9b2
Geoff Von Allmen <geoff@ibleducation.com>
2021-06-07 19:49:20 +0000

    Add OauthProvidersViewset to apiviews

--- ibl_third_party_auth/api/views.py
+++ ibl_third_party_auth/api/views.py
@@ -16,6 +16,9 @@ from rest_framework import exceptions, permissions, status, throttling
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import viewsets, mixins
+from rest_framework.permissions import IsAdminUser
+
 from social_django.models import UserSocialAuth
 
 from openedx.core.lib.api.authentication import (
@@ -23,11 +26,15 @@ from openedx.core.lib.api.authentication import (
     BearerAuthenticationAllowInactiveUser
 )
 from openedx.core.lib.api.permissions import ApiKeyHeaderPermission
+from openedx.core.lib.api.view_utils import view_auth_classes
+
 from common.djangoapps.third_party_auth import pipeline
 from common.djangoapps.third_party_auth.api import serializers
 from common.djangoapps.third_party_auth.api.permissions import TPA_PERMISSIONS
 from common.djangoapps.third_party_auth.provider import Registry
 from common.djangoapps.third_party_auth.api.utils import filter_user_social_auth_queryset_by_provider
+from common.djangoapps.third_party_auth.models import OAuth2ProviderConfig
+
 
 
 class ProviderBaseThrottle(throttling.UserRateThrottle):
@@ -434,3 +441,43 @@ class ThirdPartyAuthUserStatusView(APIView):
                 })
 
         return Response(tpa_states)
+
+
+class CreateReadListViewset(mixins.CreateModelMixin,
+                            mixins.ListModelMixin,
+                            mixins.RetrieveModelMixin,
+                            viewsets.GenericViewSet):
+    """Base class for providing Create, List, and Retrieve object methods"""
+    pass
+
+
+@view_auth_classes(is_authenticated=True)
+class OAuthProvidersViewset(CreateReadListViewset):
+    """API viewset to dynamically Create/List/Retrieve OAuth2 Clients"""
+    serializer_class = serializers.OAuthProviderSerializer
+    authentication_classes = [BearerAuthentication]
+    permission_classes = [IsAdminUser]
+    pagination_class = None
+
+    def get_queryset(self):
+        """Return most recent config for each slug/site combo for backend"""
+        queryset = OAuth2ProviderConfig.objects.current_set().order_by('site__domain')
+        queryset = queryset.filter(backend_name=self.kwargs.get('backend'))
+        return queryset
+
+    def get_serializer_context(self):
+        """Add list of required field for other_settings
+
+        These are mostly only required for keycloak, which is what we are
+        implementing this for. This could be extended or changed for other
+        backends
+        """
+        context = super(OAuthProvidersViewset, self).get_serializer_context()
+        context['required_other_settings'] = [
+            'AUTHORIZATION_URL',
+            'ACCESS_TOKEN_URL',
+            'PUBLIC_KEY',
+            'END_SESSION_URL',
+            'ISS',
+        ]
+        return context
