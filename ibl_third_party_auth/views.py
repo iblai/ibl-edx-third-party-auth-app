f3b322ad10b70702521b7aa1a13ac5ef6a75651a
Geoff Von Allmen <geoff@ibleducation.com>
2021-06-09 16:56:41 +0000

    Drop unused methods in views; fix logout

--- ibl_third_party_auth/views.py
+++ ibl_third_party_auth/views.py
@@ -5,173 +5,29 @@ Extra views required for SSO
 import logging
 from importlib import import_module
 
-from urllib import urlencode
+from urllib.parse import urlencode
 
-import edx_oauth2_provider
-
-from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django.conf import settings
-from django.http import Http404, HttpResponse, HttpResponseNotAllowed, HttpResponseNotFound, HttpResponseServerError
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.base import View
 from social_core.utils import setting_name
-from social_django.utils import load_backend, load_strategy, psa
-from social_django.views import complete
 from social_django.models import UserSocialAuth
 
 from openedx.core.djangoapps.user_authn.views.logout import LogoutView
 from openedx.core.djangoapps.user_authn.cookies import delete_logged_in_cookies
 
-from common.djangoapps import third_party_auth
-from common.djangoapps.student.helpers import get_next_url_for_login_page
-from common.djangoapps.student.models import UserProfile
-from common.djangoapps.student.views import compose_and_send_activation_email
-from common.djangoapps.third_party_auth import pipeline, provider
+from common.djangoapps.third_party_auth import pipeline as tpa_pipeline, provider
 
 from ibl_third_party_auth import backchannel_logout
 
-from .models import SAMLConfiguration, SAMLProviderConfig
 
 log = logging.getLogger(__name__)
 
-URL_NAMESPACE = getattr(settings, setting_name('URL_NAMESPACE'), None) or 'social'
 TPA_LOGOUT_PROVIDER = getattr(settings, 'TPA_LOGOUT_PROVIDER', None)
 TPA_POST_LOGOUT_REDIRECT_FIELD = getattr(settings, 'TPA_POST_LOGOUT_REDIRECT_FIELD', 'redirect_uri')
 TPA_POST_LOGOUT_REDIRECT_URL = getattr(settings, 'TPA_POST_LOGOUT_REDIRECT_URL', 'current_site')
-SESSIONS_ENGINE = import_module(settings.SESSION_ENGINE)
-
-
-def inactive_user_view(request):
-    """
-    A newly or recently registered user has completed the social auth pipeline.
-    Their account is not yet activated, but we let them login since the third party auth
-    provider is trusted to vouch for them. See details in pipeline.py.
-
-    The reason this view exists is that if we don't define this as the
-    SOCIAL_AUTH_INACTIVE_USER_URL, inactive users will get sent to LOGIN_ERROR_URL, which we
-    don't want.
-
-    If the third_party_provider.skip_email_verification is set then the user is activated
-    and verification email is not sent
-    """
-    # 'next' may be set to '/account/finish_auth/.../' if this user needs to be auto-enrolled
-    # in a course. Otherwise, just redirect them to the dashboard, which displays a message
-    # about activating their account.
-    user = request.user
-    profile = UserProfile.objects.get(user=user)
-    activated = user.is_active
-    # If the user is registering via 3rd party auth, track which provider they use
-    if third_party_auth.is_enabled() and pipeline.running(request):
-        running_pipeline = pipeline.get(request)
-        third_party_provider = provider.Registry.get_from_pipeline(running_pipeline)
-        if third_party_provider.skip_email_verification and not activated:
-            user.is_active = True
-            user.save()
-            activated = True
-    if not activated:
-        compose_and_send_activation_email(user, profile)
-
-    return redirect(request.GET.get('next', 'dashboard'))
-
-
-def saml_metadata_view(request):
-    """
-    Get the Service Provider metadata for this edx-platform instance.
-    You must send this XML to any Shibboleth Identity Provider that you wish to use.
-    """
-    idp_slug = request.GET.get('tpa_hint', None)
-    saml_config = 'default'
-    if idp_slug:
-        idp = SAMLProviderConfig.current(idp_slug)
-        if idp.saml_configuration:
-            saml_config = idp.saml_configuration.slug
-    if not SAMLConfiguration.is_enabled(request.site, saml_config):
-        raise Http404
-    complete_url = reverse('social:complete', args=("tpa-saml", ))
-    if settings.APPEND_SLASH and not complete_url.endswith('/'):
-        complete_url = complete_url + '/'  # Required for consistency
-    saml_backend = load_backend(load_strategy(request), "tpa-saml", redirect_uri=complete_url)
-    metadata, errors = saml_backend.generate_metadata_xml(idp_slug)
-
-    if not errors:
-        return HttpResponse(content=metadata, content_type='text/xml')
-    return HttpResponseServerError(content=', '.join(errors))
-
-
-@csrf_exempt
-@psa('{0}:complete'.format(URL_NAMESPACE))
-def lti_login_and_complete_view(request, backend, *args, **kwargs):
-    """This is a combination login/complete due to LTI being a one step login"""
-
-    if request.method != 'POST':
-        return HttpResponseNotAllowed('POST')
-
-    request.backend.start()
-    return complete(request, backend, *args, **kwargs)
-
-
-def post_to_custom_auth_form(request):
-    """
-    Redirect to a custom login/register page.
-
-    Since we can't do a redirect-to-POST, this view is used to pass SSO data from
-    the third_party_auth pipeline to a custom login/register form (possibly on another server).
-    """
-    pipeline_data = request.session.pop('tpa_custom_auth_entry_data', None)
-    if not pipeline_data:
-        raise Http404
-    # Verify the format of pipeline_data:
-    data = {
-        'post_url': pipeline_data['post_url'],
-        # data: The provider info and user's name, email, etc. as base64 encoded JSON
-        # It's base64 encoded because it's signed cryptographically and we don't want whitespace
-        # or ordering issues affecting the hash/signature.
-        'data': pipeline_data['data'],
-        # The cryptographic hash of user_data:
-        'hmac': pipeline_data['hmac'],
-    }
-    return render(request, 'third_party_auth/post_custom_auth_entry.html', data)
-
-
-class IdPRedirectView(View):
-    """
-    Redirect to an IdP's login page if the IdP exists; otherwise, return a 404.
-
-    Example usage:
-
-        GET auth/idp_redirect/saml-default
-
-    """
-    def get(self, request, *args, **kwargs):
-        """
-        Return either a redirect to the login page of an identity provider that
-        corresponds to the provider_slug keyword argument or a 404 if the
-        provider_slug does not correspond to an identity provider.
-
-        Args:
-            request (HttpRequest)
-
-        Keyword Args:
-            provider_slug (str): a slug corresponding to a configured identity provider
-
-        Returns:
-            HttpResponse: 302 to a provider's login url if the provider_slug kwarg matches an identity provider
-            HttpResponse: 404 if the provider_slug kwarg does not match an identity provider
-        """
-        # this gets the url to redirect to after login/registration/third_party_auth
-        # it also handles checking the safety of the redirect url (next query parameter)
-        # it checks against settings.LOGIN_REDIRECT_WHITELIST, so be sure to add the url
-        # to this setting
-        next_destination_url = get_next_url_for_login_page(request)
-
-        try:
-            url = pipeline.get_login_url(kwargs['provider_slug'], pipeline.AUTH_ENTRY_LOGIN, next_destination_url)
-            return redirect(url)
-        except ValueError:
-            return HttpResponseNotFound()
 
 
 class TPALogoutView(LogoutView):
@@ -189,12 +45,12 @@ class TPALogoutView(LogoutView):
         # We do not log here, because we have a handler registered to perform logging on successful logouts.
         request.is_from_logout = True
 
-        # Get the list of authorized clients before we clear the session.
-        self.oauth_client_ids = request.session.get(edx_oauth2_provider.constants.AUTHORIZED_CLIENTS_SESSION_KEY, [])
+        # Get third party auth provider's logout url
+        self.tpa_logout_url = tpa_pipeline.get_idp_logout_url_from_running_pipeline(request)
 
         logout(request)
 
-        if settings.PROJECT_ROOT.name == 'lms':
+        if settings.ROOT_URLCONF.startswith('lms'):
             # For the LMS, we redirect to the normal logout page
             response = super(LogoutView, self).dispatch(request, *args, **kwargs)
         else:
@@ -223,37 +79,11 @@ class TPALogoutView(LogoutView):
     def get_context_data(self, **kwargs):
         context = super(TPALogoutView, self).get_context_data(**kwargs)
         # Default behavior if not logoout provider set
-        if TPA_LOGOUT_PROVIDER is None:
+        if TPA_LOGOUT_PROVIDER is None or not self.tpa_logout_url:
             return context
 
-        backend = provider.Registry.get_from_pipeline(
-            {'backend': TPA_LOGOUT_PROVIDER})
-
-        # Default behavior if specified backend isn't found
-        if not backend:
-            log.error(
-                'Expected backend from TPA_LOGOUT_PROVIDER: %s not found '
-                'for site %s; defaulting to normal logout behavior',
-                TPA_LOGOUT_PROVIDER, self.request.site.domain)
-            return context
-
-        relogin = self.request.GET.get('relogin')
-
-        # Want to log user out and redirect them to their providers login
-        # This is used when a user's OP session ends via check-session-iframe
-        if relogin is not None:
-            login_url = pipeline.get_login_url(
-                # TODO: Fix the redirect URL to be more appropriate?
-                backend.provider_id,
-                'login',
-                redirect_url=self.request.GET.get('next', '/'))
-            context['target'] = login_url
-            return context
-
-        # Use the end session endpoint as the redirect target
-        end_session_url = self._get_end_session_url(backend)
-        end_session_url = self._add_post_logout_redirect_uri(end_session_url)
-        context['target'] = end_session_url if end_session_url else context['target']
+        end_session_url = self._add_post_logout_redirect_uri(self.tpa_logout_url)
+        context['target'] = end_session_url
         return context
 
     def _get_end_session_url(self, backend):
@@ -278,7 +108,8 @@ class TPALogoutView(LogoutView):
             return end_session_url
 
         if TPA_POST_LOGOUT_REDIRECT_URL == 'current_site':
-            url = 'https://{}'.format(self.request.site.domain)
+            protocol = 'https' if self.request.is_secure() else 'http'
+            url = '{}://{}'.format(protocol, self.request.site.domain)
         else:
             url = TPA_POST_LOGOUT_REDIRECT_URL
 
