96b79cb9fd9b0a951871d513256443b0a7a4baa2
Geoff Von Allmen <geoff@ibleducation.com>
2021-06-09 17:01:21 +0000

    Remove TPA urls from api/urls.py and keep ours

--- ibl_third_party_auth/api/urls.py
+++ ibl_third_party_auth/api/urls.py
@@ -1,38 +1,10 @@
-""" URL configuration for the third party auth API """
-
-
-from django.conf import settings
-from django.conf.urls import url
-
-from .views import ThirdPartyAuthUserStatusView, UserMappingView, UserView, UserViewV2
 from rest_framework.routers import DefaultRouter
 
-from .views import UserMappingView, UserView, UserViewV2, OAuthProvidersViewset
+from .views import OAuthProvidersViewset
 
 router = DefaultRouter()
 router.register(r'v0/oauth-providers/(?P<backend>[\w.+-]+)', OAuthProvidersViewset,
-                base_name='third_party_auth_oauth_providers')
+                basename='third_party_auth_oauth_providers')
 
 
-PROVIDER_PATTERN = r'(?P<provider_id>[\w.+-]+)(?:\:(?P<idp_slug>[\w.+-]+))?'
-
-urlpatterns = [
-    url(
-        r'^v0/users/{username_pattern}$'.format(username_pattern=settings.USERNAME_PATTERN),
-        UserView.as_view(),
-        name='third_party_auth_users_api',
-    ),
-    url(r'^v0/users/', UserViewV2.as_view(), name='third_party_auth_users_api_v2'),
-    url(
-        r'^v0/providers/{provider_pattern}/users$'.format(provider_pattern=PROVIDER_PATTERN),
-        UserMappingView.as_view(),
-        name='third_party_auth_user_mapping_api',
-    ),
-    url(
-        r'^v0/providers/user_status$',
-        ThirdPartyAuthUserStatusView.as_view(),
-        name='third_party_auth_user_status_api',
-    ),
-]
-
-urlpatterns += router.urls
+urlpatterns = router.urls
