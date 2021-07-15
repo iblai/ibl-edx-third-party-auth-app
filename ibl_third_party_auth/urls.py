f70432fa50980a85513285d90e60d30d3edcef92
Geoff Von Allmen <geoff@ibleducation.com>
2021-07-15 18:02:48 +0000

    Add api.urls to root url conf

--- ibl_third_party_auth/urls.py
+++ ibl_third_party_auth/urls.py
@@ -4,8 +4,11 @@ from django.conf import settings
 from django.conf.urls import url
 
 from .views import back_channel_logout
+from .api import urls as api_urls
 
 
 urlpatterns = [
     url(r'^auth/back_channel_logout/(?P<backend>\w+)$', back_channel_logout, name='tpa-backchannel-logout'),
 ]
+
+urlpatterns += api_urls.urlpatterns
