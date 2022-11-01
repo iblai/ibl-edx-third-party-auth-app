2a0de0e306b69f3d157d0246843351fc466c9fd3
Wang Zhao <wz@wangzhao.rocks>
2022-11-01 22:31:09 +0800

    try to update settings

--- ibl_third_party_auth/settings/common.py
+++ ibl_third_party_auth/settings/common.py
@@ -33,3 +33,25 @@ def plugin_settings(settings):  # pylint: disable=unused-argument
     if settings.ROOT_URLCONF.startswith('cms') and tpa not in settings.INSTALLED_APPS:
         settings.INSTALLED_APPS.append(tpa)
 
+
+    # from the plugin
+    settings.TPA_PROVIDER_BURST_THROTTLE = '10/min'
+    settings.TPA_PROVIDER_SUSTAINED_THROTTLE = '50/hr'
+    settings.IBL_TPA_MIDDLEWARE_TARGET_URLS = {'/login', '/register', '/signup', '/signin'}
+    settings.IBL_TPA_MIDDLEWARE_DEFAULT_PROVIDER = 'keycloak'
+    settings.IBL_CMS_PREVENT_CONCURRENT_LOGINS = True
+    settings.REGISTRATION_EXTRA_FIELDS = {
+        "city": "hidden",
+        "confirm_email": "hidden",
+        "country": "hidden",
+        "gender": "hidden",
+        "goals": "hidden",
+        "honor_code": "hidden",
+        "level_of_education": "hidden",
+        "mailing_address": "hidden",
+        "terms_of_service": "hidden",
+        "year_of_birth": "hidden"
+    }
+    settings.FEATURES["DISABLE_STUDIO_SSO_OVER_LMS"] = True
+    settings.FEATURES["ENABLE_THIRD_PARTY_AUTH"] = True
+
