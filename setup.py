1888ea9e1b42247dd0f45763123d82c083dacb58
Ngabidong Brian <gipsbrian@gmail.com>
2024-06-24 13:00:06 +0300

    Update setup.py
    Update to version 2.0.2 - 2.0.3

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def package_data(pkg, roots):
 
 setup(
     name='ibl-third-party-auth',
-    version='2.0.2',
+    version='2.0.3',
     install_requires=[
         "ddt",
         "social-auth-app-django",
