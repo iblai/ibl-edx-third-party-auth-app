5a2ec8360f5f0fa14639b6d621d1eb4058dfceb5
gipsbrian <gipsbrian@gmail.com>
2024-07-25 13:13:13 +0000

    add google and apple verification

--- src/ibl_third_party_auth/registration.py
+++ src/ibl_third_party_auth/registration.py
@@ -77,7 +77,7 @@ class IblUserManagementView(APIView):
             log.info(f"Claims: {claims}")
 
             # Example claim validation (you can add more as needed)
-            if claims['aud'] != settings.CLIENT:
+            if claims['aud'] != self.setting('CLIENT'):
                 return False
             if claims['exp'] < time.time():
                 return False
