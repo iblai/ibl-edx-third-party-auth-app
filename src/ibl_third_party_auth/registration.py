616505361a19d9517d23dfc0559ef87fc6e336ad
gipsbrian <gipsbrian@gmail.com>
2024-07-25 14:24:13 +0000

    add google and apple verification

--- src/ibl_third_party_auth/registration.py
+++ src/ibl_third_party_auth/registration.py
@@ -42,50 +42,6 @@ class IblUserManagementView(APIView, AppleIdAuth):
     authentication_classes = []
     permission_classes = []
 
-    # def create_user_account(self, request):
-    #     params = request.data
-    #     log.info("Params: %s", params)
-
-    #     import re
-
-    #     # Extract new parameters
-
-    #     client_id = params.get("client_id")
-    #     asymmetric_jwt = params.get("asymmetric_jwt")
-    #     token_type = params.get("token_type")
-    #     access_token = params.get("access_token")
-    #     scope = params.get("scope")
-    #     email = params.get("email")
-    #     first_name = params.get("first_name")
-    #     last_name = params.get("last_name")
-
-    #     # Generate name and username
-    #     if first_name and last_name:
-    #         name = f"{first_name} {last_name}"
-    #     elif email:
-    #         local_part = email.split('@')[0]
-    #         domain_part = email.split('@')[1].replace('.', '_')
-    #         local_part = re.sub(r'\W+', '_', local_part)  # Replace all non-alphanumeric characters with underscores
-    #         name = local_part.replace('_', ' ')
-    #         username = f"{local_part}_{domain_part}"
-    #     else:
-    #         return Response({"error": "Email is required if first name and last name are not provided."}, status=400)
-
-    #     # Update params with generated name and username
-    #     params["name"] = name
-    #     params["username"] = username
-
-    #     log.info("Updated params: %s", params)
-
-    #     # Validate request parameters
-    #     validation_response = validate_user_params(params)
-    #     if validation_response:
-    #         return validation_response
-
-    #     # Create or update user
-    #     user, user_response = create_or_update_user(params)
-    #     return user_response
-
     def post(self, request, format=None):
         """
         Create user with the manage_user command.
@@ -100,36 +56,14 @@ class IblUserManagementView(APIView, AppleIdAuth):
         last_name (optional): Last name of user
         """
         log.info("User registration request.........")
-        # params = request.data
-        # log.info("Params: %s", params)
-
         self.strategy = load_strategy(request)
-        access_token = request.data.get('access_token')
-        backend = request.data.get("backend")
-        if not access_token:
-            return Response({'error': 'Missing access_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
+        id_token = request.data.get('access_token')
+        if not id_token:
+            return Response({'error': 'Missing id_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
 
-        if backend:
-            if backend == "apple-id":
-                try:
-                    decoded_data = self.decode_id_token(access_token)
-                except Exception as e:
-                    decoded_data = None
-                    return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
-
-                if decoded_data is None:
-                    return Response({"error": "Invalid access token."}, status=400)
-
-            elif backend == "google-oauth2":
-                is_valid = self.verify_google_access_token(access_token)
-                if is_valid:
-                    return "The access_token is valid."
-            else:
-                return Response({"error": "Unsupported backend."}, status=400)
-
-
-
-
-
-
+        try:
+            decoded_data = self.decode_id_token(id_token)
+            return Response({'decoded_data': decoded_data}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
