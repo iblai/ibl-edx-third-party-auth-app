e34fbb13149a7ad4fba2e08c848bebdae8b8c7e7
gipsbrian <gipsbrian@gmail.com>
2024-07-25 21:56:10 +0000

    - update user creation

--- src/ibl_third_party_auth/utils/user.py
+++ src/ibl_third_party_auth/utils/user.py
@@ -2,6 +2,7 @@ import logging
 
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
+from ibl_user_management_api.utils.main import retrieve_user
 
 log = logging.getLogger(__name__)
 
@@ -14,15 +15,12 @@ class UserUtils():
         Create a new user
         """
         user, created = self.user_model.objects.get_or_create(username=username, email=email, first_name=first_name, last_name=last_name)
-        log.info("User created: %s", user)
         if created:
-            # Create user profile
-            profile = self.profile_model(user=user)
+            user = retrieve_user(username)
             if first_name and last_name:
-                profile.name = f"{first_name}_{last_name}"
+                user.profile.name = f"{first_name}_{last_name}"
             else:
-                profile.name = username
-            profile.save()
+                user.profile.name = username
             user.save()
             return True
         else:
