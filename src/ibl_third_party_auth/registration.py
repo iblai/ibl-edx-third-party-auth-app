fc82d7470f3856d240bda07e4c79bec675c570a2
gipsbrian <gipsbrian@gmail.com>
2024-07-25 17:54:06 +0000

    add google jwt validation

--- src/ibl_third_party_auth/registration.py
+++ src/ibl_third_party_auth/registration.py
@@ -16,6 +16,7 @@ from jose.utils import base64url_decode
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from social_core.backends.open_id_connect import OpenIdConnectAuth
 from social_django.utils import load_strategy
 
 from ibl_third_party_auth.patches.patch_apple_id import IBLAppleIdAuth
@@ -56,15 +57,11 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
         try:
             # Decode the JWT header to get the Key ID (kid)
             header = jwt.get_unverified_header(access_token)
-            log.info(f"Header: {header}")
             kid = header['kid']
-            log.info(f"Key ID: {kid}")
 
             # Get the public key from Apple's JWKs
             jwk_key = self.get_apple_jwk(kid)
-            log.info(f"JWK: {jwk_key}")
             public_key = jwk.construct(jwk_key)
-            log.info(f"Public key: {public_key}")
 
             # Verify the JWT signature
             message, encoded_signature = access_token.rsplit('.', 1)
@@ -74,14 +71,6 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
                 return False
 
             claims = jwt.get_unverified_claims(access_token)
-            log.info(f"Claims: {claims}")
-
-            log.info(f"aud: {claims['aud']}")
-            log.info(f"audience settings: {self.setting('AUDIENCE')}")
-
-            log.info(f"client: {self.setting('CLIENT')}")
-            log.info(f"Expiration time: {claims['exp']}")
-
 
             if isinstance(self.setting('AUDIENCE'), list):
                 if claims['aud'] not in self.setting('AUDIENCE'):
@@ -97,6 +86,10 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
             print(f"Token verification failed: {e}")
             return False
 
+    def verify_google_access_token(self, id_token, access_token):
+        opene_id_connect = OpenIdConnectAuth()
+        return opene_id_connect.verify_and_decode_id_token(id_token, access_token)
+
     def post(self, request, format=None):
         """
         Create user with the manage_user command.
@@ -111,20 +104,39 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
         last_name (optional): Last name of user
         """
         self.strategy = load_strategy(request)
-        id_token = request.data.get('access_token')
-        log.info(f"id_token: {id_token}" )
-        if not id_token:
-            return Response({'error': 'Missing id_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
-        try:
-            decoded_data = self.verify_apple_access_token(id_token)
-            log.info(f"Decoded data: {decoded_data}")
-            if not decoded_data:
-                return Response({'error': 'access_token could not be verified'}, status=status.HTTP_400_BAD_REQUEST)
-            create_user = self.create_user_account(request)
+        access_token = request.data.get('access_token')
+        log.info(f"access_token: {access_token}" )
+        backend = request.data.get("backend")
 
-            return create_user
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        if backend == "apple-id":
+            if not access_token:
+                return Response({'error': 'Missing access_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                decoded_data = self.verify_apple_access_token(access_token)
+                log.info(f"Decoded data: {decoded_data}")
+                if not decoded_data:
+                    return Response({'error': 'access_token could not be verified'}, status=status.HTTP_400_BAD_REQUEST)
+                create_user = self.create_user_account(request)
+
+                return create_user
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        elif backend == "google-oauth2":
+            id_token = request.data.get('id_token')
+            if not id_token:
+                return Response({'error': 'Missing id_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
+            try:
+                decoded_data = self.verify_google_access_token(id_token, access_token)
+                log.info(f"Decoded data: {decoded_data}")
+                if not decoded_data:
+                    return Response({'error': 'id_token could not be verified'}, status=status.HTTP_400_BAD_REQUEST)
+                create_user = self.create_user_account(request)
+
+                return create_user
+            except Exception as e:
+                return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'error': 'Invalid backend'}, status=status.HTTP_400_BAD_REQUEST)
 
     def create_user_account(self, request):
         params = request.data
