88ea03276baec7ff2c016bbdf57862c85a25de4b
gipsbrian <gipsbrian@gmail.com>
2024-07-26 17:20:10 +0000

    - add options to decode function

--- src/ibl_third_party_auth/registration.py
+++ src/ibl_third_party_auth/registration.py
@@ -1,6 +1,7 @@
 """
 User API views
 """
+
 import logging
 import time
 
@@ -25,10 +26,12 @@ from ibl_third_party_auth.utils.user import UserUtils
 
 log = logging.getLogger(__name__)
 
+
 class IblUserManagementView(APIView, IBLAppleIdAuth):
     """
     User API extension.
     """
+
     authentication_classes = []
     permission_classes = []
     JWK_URL = "https://appleid.apple.com/auth/keys"
@@ -46,16 +49,16 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
     TOKEN_TTL_SEC = 6 * 30 * 24 * 60 * 60
 
     GOOGLE_JWK_URL = "https://www.googleapis.com/oauth2/v3/certs"
-    GOOGLE_AUDIENCE = getattr(settings, 'SOCIAL_AUTH_GOOGLE_CLIENT_ID', None)
+    GOOGLE_AUDIENCE = getattr(settings, "SOCIAL_AUTH_GOOGLE_CLIENT_ID", None)
 
     def get_google_jwk(self, kid):
         log.info(f"Getting Google JWK for kid: {kid}")
         response = requests.get(self.GOOGLE_JWK_URL)
         response.raise_for_status()
-        keys = response.json().get('keys')
+        keys = response.json().get("keys")
 
         for key in keys:
-            if key['kid'] == kid:
+            if key["kid"] == kid:
                 return key
         raise ValueError("Key ID not found in Google's JWKs")
 
@@ -65,7 +68,7 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
             # Decode the JWT header to get the Key ID (kid)
             header = jwt.get_unverified_header(id_token)
             log.info(f"Header: {header}")
-            kid = header['kid']
+            kid = header["kid"]
             log.info(f"Kid: {kid}")
 
             # Get the public key from Google's JWKs
@@ -79,14 +82,22 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
             # Verify the JWT signature and decode the token
             try:
                 log.info(f"Decoding token with public key: {public_key}")
-                claims = jwt.decode(token=id_token, key=public_key, access_token=access_token,  algorithms=['RS256'], audience=self.GOOGLE_AUDIENCE, issuer='https://accounts.google.com')
+                claims = jwt.decode(
+                    token=id_token,
+                    key=public_key,
+                    access_token=access_token,
+                    algorithms=["RS256"],
+                    audience=self.GOOGLE_AUDIENCE,
+                    issuer="https://accounts.google.com",
+                    options={"verify_sub": False, "verify_jti": False, "verify_at_hash": False},
+                )
                 log.info(f"Claims: {claims}")
             except Exception as e:
                 log.error(f"Error decoding token: {e}")
                 return False
 
             # Check the expiration
-            if claims['exp'] < time.time():
+            if claims["exp"] < time.time():
                 return False
 
             return True
@@ -97,10 +108,10 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
     def get_apple_jwk(self, kid):
         response = requests.get(self.JWK_URL)
         response.raise_for_status()
-        keys = response.json().get('keys')
+        keys = response.json().get("keys")
 
         for key in keys:
-            if key['kid'] == kid:
+            if key["kid"] == kid:
                 return key
         raise ValueError("Key ID not found in Apple's JWKs")
 
@@ -108,26 +119,26 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
         try:
             # Decode the JWT header to get the Key ID (kid)
             header = jwt.get_unverified_header(access_token)
-            kid = header['kid']
+            kid = header["kid"]
 
             # Get the public key from Apple's JWKs
             jwk_key = self.get_apple_jwk(kid)
             public_key = jwk.construct(jwk_key)
 
             # Verify the JWT signature
-            message, encoded_signature = access_token.rsplit('.', 1)
-            decoded_signature = base64url_decode(encoded_signature.encode('utf-8'))
-            if not public_key.verify(message.encode('utf-8'), decoded_signature):
+            message, encoded_signature = access_token.rsplit(".", 1)
+            decoded_signature = base64url_decode(encoded_signature.encode("utf-8"))
+            if not public_key.verify(message.encode("utf-8"), decoded_signature):
                 return False
 
             claims = jwt.get_unverified_claims(access_token)
-            if isinstance(self.setting('AUDIENCE'), list):
-                if claims['aud'] not in self.setting('AUDIENCE'):
+            if isinstance(self.setting("AUDIENCE"), list):
+                if claims["aud"] not in self.setting("AUDIENCE"):
                     return False
             else:
-                if claims['aud'] != self.setting('AUDIENCE'):
+                if claims["aud"] != self.setting("AUDIENCE"):
                     return False
-            if claims['exp'] < time.time():
+            if claims["exp"] < time.time():
                 return False
 
             return True
@@ -152,46 +163,77 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
         backend = request.data.get("backend")
 
         if backend == "apple-id":
-            id_token = request.data.get('access_token')
+            id_token = request.data.get("access_token")
             if not id_token:
-                return Response({'error': 'Missing id_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Missing id_token parameter"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             try:
                 decoded_data = self.verify_apple_access_token(id_token)
                 if not decoded_data:
-                    return Response({'error': 'access_token could not be verified'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {"error": "access_token could not be verified"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
                 create_user = self.create_user_account(request)
                 if create_user:
-                    return Response({'message': 'Account created successfully'}, status=status.HTTP_200_OK)
+                    return Response(
+                        {"message": "Account created successfully"},
+                        status=status.HTTP_200_OK,
+                    )
                 else:
-                    return Response({'error': 'Account could not be created'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {"error": "Account could not be created"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
             except Exception as e:
                 log.error("Error creating user: %s", e)
-                return Response({'error': "Account could not be created"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Account could not be created"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
         elif backend == "google-oauth2":
-            id_token = request.data.get('id_token')
-            access_token = request.data.get('access_token')
+            id_token = request.data.get("id_token")
+            access_token = request.data.get("access_token")
             if not id_token:
-                return Response({'error': 'Missing id_token parameter'}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Missing id_token parameter"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             try:
                 decoded_data = self.verify_google_jwt_token(id_token, access_token)
                 log.info(f"Decoded data: {decoded_data}")
                 if not decoded_data:
-                    return Response({'error': 'id_token could not be verified'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {"error": "id_token could not be verified"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
                 create_user = self.create_user_account(request)
 
                 if create_user:
-                    return Response({'message': 'Account created successfully'}, status=status.HTTP_200_OK)
+                    return Response(
+                        {"message": "Account created successfully"},
+                        status=status.HTTP_200_OK,
+                    )
                 else:
-                    return Response({'error': 'Account could not be created'}, status=status.HTTP_400_BAD_REQUEST)
+                    return Response(
+                        {"error": "Account could not be created"},
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
             except Exception as e:
                 log.error("Error creating user: %s", e)
-                return Response({'error': "Account could not be created"}, status=status.HTTP_400_BAD_REQUEST)
+                return Response(
+                    {"error": "Account could not be created"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
         else:
-            return Response({'error': 'Invalid backend'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"error": "Invalid backend"}, status=status.HTTP_400_BAD_REQUEST
+            )
 
     def create_user_account(self, request):
         params = request.data
-
 
         import re
 
@@ -200,9 +242,9 @@ class IblUserManagementView(APIView, IBLAppleIdAuth):
         last_name = params.get("last_name")
 
         if email:
-            local_part = email.split('@')[0]
-            domain_part = email.split('@')[1].replace('.', '_')
-            local_part = re.sub(r'\W+', '_', local_part)
+            local_part = email.split("@")[0]
+            domain_part = email.split("@")[1].replace(".", "_")
+            local_part = re.sub(r"\W+", "_", local_part)
             username = f"{local_part}_{domain_part}"
             if not first_name:
                 first_name = local_part
